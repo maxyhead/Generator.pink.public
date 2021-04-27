724117a0c352734a67b02be59ae7fedfd1e86b96
Lex Calebout <lexcalebout@Lexs-Mac-mini.local>
2021-04-27 14:10:49 +0200

    testpush

---
+++ packages/react-app/src/components/buttons/Button.styles.js
@@ -0,0 +1,49 @@
+import { theme } from '../../theme'
+import { 
+  makeStyles,
+} from '@material-ui/core/styles';
+
+export const useStyles = makeStyles(() => ({
+    header: {
+        paddingTop: '10px',
+        backgroundColor: theme.palette.background.main,
+        minHeight:'5vh'
+    },
+    title: {
+        display: 'none',
+        color: theme.palette.text.main,
+
+        fontVariantCaps: 'all-small-caps',
+        '&:hover': {
+            color: theme.palette.primary,
+        },
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },},
+    wrapper: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+    },
+    button: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: '30px',
+    },
+    nowrapper: {
+        display: 'flex',
+        flexWrap: 'nowrap',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    modal: {
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'fixed'
+    },
+   
+}));
